824f76860fa88af7da331671c6aca6e8e7165839
silviaferraris <silvia1.ferraris@mail.polimi.it>
2019-10-06 12:06:39 +0200

    ci sono ancora dei problemi...

---
+++ public/assets/js/typewriter.js
@@ -0,0 +1,19 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+document.addEventListener('DOMContentLoaded', init);
+$(document).ready(function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    //Init typewriter
+    new TypeWriter(txtElement, words, wait);
+}
+
