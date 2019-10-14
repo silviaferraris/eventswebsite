7932707d965a05563584975035ee49eac6bafca2
silviaferraris <silvia1.ferraris@mail.polimi.it>
2019-10-14 16:52:30 +0200

    new navbar

--- public/assets/js/navbar_and_footer_loader.js
+++ public/assets/js/navbar_and_footer_loader.js
@@ -1,5 +1,31 @@
 $(document).ready(function()
 {
-    $("#navbar").load("/navbar.html");
+    let scrollPoint;
+
+    $("#navbar").load("/navbar.html", () =>
+    {
+        $(document).scroll(function ()
+        {
+            let currentScrollPoint = $(this).scrollTop();
+
+            if(currentScrollPoint > 850)
+            {
+                if(!$("#navbar nav").hasClass("small-fixed")) $("#navbar nav").addClass("small-fixed");
+            }
+            else
+            {
+                $(".small-fixed").removeClass("small-fixed");
+            }
+
+            if(currentScrollPoint > scrollPoint) //scrolling down
+            {
+
+            }
+            else if(currentScrollPoint < scrollPoint) //scrolling up
+            {
+
+            }
+        });
+    });
     $("#footer").load("/footer.html");
 });
