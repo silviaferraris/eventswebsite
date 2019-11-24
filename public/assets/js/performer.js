9c949781b684d458322539115c9067c65db82e64
silviaferraris <silvia1.ferraris@mail.polimi.it>
2019-11-24 13:28:23 +0100

    modifying shopping cart...

--- public/assets/js/performer.js
+++ public/assets/js/performer.js
@@ -1,1 +1,10 @@
-import Performer from
+import Performer from '../../assets/js/modules/performer.mjs'
+
+$(document).ready(() =>
+{
+    Performer.getAllPerformers().then(performers =>{
+        for(let p of performers){
+            console.log(p.photo);
+        }
+    })
+});
