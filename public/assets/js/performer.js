b4ef9791d6b3feb350b6037254832aeda065d617
silviaferraris <silvia1.ferraris@mail.polimi.it>
2019-12-29 01:55:23 +0100

    .

--- public/assets/js/performer.js
+++ public/assets/js/performer.js
@@ -2,9 +2,9 @@ import Performer from '../../assets/js/modules/performer.mjs'
 
 $(document).ready(() =>
 {
-    Performer.getAllPerformers().then(performers =>{
+    /*Performer.getAllPerformers().then(performers =>{
         for(let p of performers){
             console.log(p.photo);
         }
-    })
+    })*/
 });
