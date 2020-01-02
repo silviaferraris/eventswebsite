461dbeb832859237cef2e934d59839c3007d3177
silviaferraris <silvia1.ferraris@mail.polimi.it>
2020-01-02 21:04:56 +0100

    new calendar

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,4 +1,48 @@
-window.onload = function () {
+let monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let daysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function initCalendar()
+{
+    let now = new Date();
+
+    $('.calendarpicture .day').text(`${now.getDate()}, ${getDayName(now.getDay())}`);
+    $('.calendarpicture .month').text(`${getMothName(now.getMonth())}`);
+
+    let days = $('.calendarnumber');
+
+    let week = 0;
+    for(let i = 0; i < 31; i++)
+    {
+        let date = new Date(`${now.getFullYear()}-${now.getMonth()+1}-${i+1}`);
+        if(date.getMonth() > now.getMonth())break;
+
+        let dayPosition = date.getDay() === 0 ? 6 : date.getDay() - 1;
+        let currentPosition = days[week*7 + dayPosition];
+        currentPosition.innerText = date.getDate();
+        currentPosition.classList.remove('empty');
+        if(date.getTime() === new Date(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`).getTime())currentPosition.classList.add('current-day');
+
+        if(date.getDay() === 0) week++;
+    }
+
+}
+
+function getDayName(dayNumber)
+{
+    if(dayNumber < 0 || dayNumber > 6)return 'Invalid day';
+    return daysName[dayNumber];
+}
+
+function getMothName(monthNumber)
+{
+    if(monthNumber < 0 || monthNumber > 11)return 'Invalid month';
+    return monthsName[monthNumber];
+}
+
+$(initCalendar());
+
+
+/*window.onload = function () {
     let date= new Date();
     let month_name = [' January', 'February', 'March', 'April', 'May', 'June'
     ,'July', 'August', 'September', 'October', 'November', 'December'];
@@ -13,9 +57,9 @@ window.onload = function () {
     let calendar= getCalendar(day_num,days);
     document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+ year;
     document.getElementById("calendar-dates").appendChild(calendar);
+};*/
 
-}
-function getCalendar(day_num, days) {
+/*function getCalendar(day_num, days) {
     let table = document.createElement('table');
     let tr = document.createElement('tr');
     //row for the day letters
@@ -61,4 +105,4 @@ function getCalendar(day_num, days) {
         table.appendChild(tr);
     }
     return table;
-}
+}*/
